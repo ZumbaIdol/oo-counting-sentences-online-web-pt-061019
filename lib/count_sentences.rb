ad9b502d0397ce52be47977845517986a7f1d152
David Foote <david.a.foote@gmail.com>
2019-06-20 00:01:39 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).count
+    #self.split(/[.!?]/).count
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
   end
 end
